535c0045ec8cb0b45ed82d92bb31171128a33f12
Léo Chely <31514354+leochely@users.noreply.github.com>
2018-05-26 14:36:29 +0200

    php updated

--- contact.php
+++ contact.php
@@ -1,9 +1,33 @@
 <?php
-	$name = $_POST['userName'];
-	$email = $_POST['userEmail'];
-	$message = $_POST['userMessage'];
-	
-	echo $name;
-	
-	mail("leo.chely@live.fr", "Nouveau message sur ma page perso par" + $name,$message + "/n" + $email); 
+$errors = '';
+$myemail = 'leo.chely@live.fr'
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: contact-form-thank-you.html');
+}
 ?>
